4b868f25c5d40345abeebf180ae7e3cedcc1ffe2
DrReetuSharma <162538254+DrReetuSharma@users.noreply.github.com>
2024-09-18 15:33:15 +0530

    Update llm_langchain.py

--- llm_langchain.py
+++ llm_langchain.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai.chat_models import ChatOpenAI
+from langchain.llms import OpenAI  # or chat_models.ChatOpenAI depending on your version
 
 st.title("🔗 BioChemConversation App")
 
@@ -7,17 +7,22 @@ openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 
 def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    st.info(model.invoke(input_text))
+    if openai_api_key.startswith("sk-"):
+        model = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+        response = model.predict(input_text)
+        st.info(response)
+    else:
+        st.warning("Please enter a valid OpenAI API key!", icon="⚠")
 
 
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
-        "What is Lipinksi's rule?",
+        "What is Lipinski's rule?",
     )
-    submitted = st.form_submit_button("Let us Submit")
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    if submitted and openai_api_key.startswith("sk-"):
-        generate_response(text)
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        if openai_api_key.startswith("sk-"):
+            generate_response(text)
+        else:
+            st.warning("Please enter your OpenAI API key!", icon="⚠")
